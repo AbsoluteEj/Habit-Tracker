b9a3f0cb9c26c8e203b4e5d17a884969de8434ce
AbsoluteEj <ejsadiarin@gmail.com>
2022-08-06 00:21:40 +0800

    create method (insert record in database) & scope

--- habit-tracker/Program.cs
+++ habit-tracker/Program.cs
@@ -5,11 +5,11 @@ namespace habit_tracker
 {
     class Program
     {
+        static string connectionString = @"Data Source=habit-Tracker.db";
         static void Main(string[] args)
         {
 
             // Create Database
-            string connectionString = @"Data Source=habit-Tracker.db";
 
             using (var connection = new SqliteConnection(connectionString))
             {
@@ -27,9 +27,9 @@ namespace habit_tracker
 
                 connection.Close();
             }
+            GetUserInput();
         }
         // =========== Main Menu ============== //
-        GetUserInput();
         static void GetUserInput()
         {
             Console.Clear();
@@ -59,18 +59,18 @@ namespace habit_tracker
                         Console.WriteLine("Thank you for using!");
                         isRunning = false;
                         break;
-                    case "1":
-                        Retrieve();
-                        break;
+                    // case "1":
+                    //     Retrieve();
+                    //     break;
                     case "2":
                         Create();
                         break;
-                    case "3":
-                        Update();
-                        break;
-                    case "4":
-                        Delete();
-                        break;
+                    // case "3":
+                    //     Update();
+                    //     break;
+                    // case "4":
+                    //     Delete();
+                    //     break;
                     default:
                         Console.WriteLine("Invalid Input! Please only type from 0 - 4."); // can create new method when return back to main menu
                         break;
@@ -82,7 +82,21 @@ namespace habit_tracker
         private static void Create()
         {
             string date = GetDateInput();
+            int quantity = GetNumberInput("Enter the quantity of glasses or your unit of measure in the day (no decimal allowed). Type B to return to main menu.\n");
 
+            // Put inputs in database
+            using (var connection = new SqliteConnection(connectionString))
+            {
+                connection.Open();
+                var tableCmd = connection.CreateCommand();
+
+                tableCmd.CommandText =
+                    $"INSERT INTO drink_water(date, quantity) VALUES ({date}, {quantity})";
+
+                tableCmd.ExecuteNonQuery();
+
+                connection.Close();
+            }
         }
 
         internal static string GetDateInput()
@@ -93,6 +107,23 @@ namespace habit_tracker
 
             if (dateInput == "0") GetUserInput();
 
+            return dateInput;
+
+        }
+
+        internal static int GetNumberInput(string message)
+        {
+            Console.WriteLine(message);
+            // can be: (without string message parameter & argument)
+            // Console.WriteLine("Enter the quantity of glasses or your unit of measure in the day. Type B to return to main menu.\n");
+
+            string quantityInput = Console.ReadLine();
+
+            if (quantityInput.ToUpper() == "B") GetUserInput();
+
+            int finalQuantity = Convert.ToInt32(quantityInput);
+
+            return finalQuantity;
         }
     }
 }
